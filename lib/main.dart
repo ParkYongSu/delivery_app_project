f7a71a3ffeb2d07fe29403d68a404271c4be1691
ParkYongSu <71327204+ParkYongSu@users.noreply.github.com>
2023-02-23 06:21:32 +0900

    FEAT: 로그인 화면 ui

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:delivery_app_project/common/component/custom_text_field.dart';
+import 'package:delivery_app_project/user/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const _App()
+  );
+}
+
+class _App extends StatelessWidget {
+  const _App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: "NotoSans"
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
+
